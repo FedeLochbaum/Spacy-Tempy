47fae4a594d7c97feb8cdce96a48712ec5aa26a3
Federico Lochbaum <federico.lochbaum@gmail.com>
2017-03-31 15:42:40 -0300

    server concurrente, levantar muchos process lo hace mas lento

--- TP2/rudy.erl
+++ TP2/rudy.erl
@@ -1,30 +1,56 @@
 -module(rudy).
--export([init/1,handler/1,request/1,reply/1,start/1, stop/0]).
+-export([init/2,handler/1,request/1,reply/1,start/2, stop/0,registers_handlers/3,throwHandler/2,abortPids/1,abort/0]).
 -import(http,[parse_request/1,ok/1]).
 
-start(Port) ->
-  register(rudy, spawn(fun() -> init(Port) end)).
+-define(Pids, []).
+
+
+start(Port,NUM) ->
+  register(rudy, spawn(fun() -> init(Port,NUM) end)).
 
 stop() ->
-  exit(whereis(rudy), "time to die").
+  rudy ! close.
+  %exit(whereis(rudy), "time to die").
+
+abort() -> receive
+  close -> abortPids(?Pids)
+end.
 
 
-init(Port) ->
+abortPids ([]) ->
+  ok;
+abortPids([X |XS]) ->
+  exit(whereis(X), "time to die"),
+  abortPids(XS).
+
+
+init(Port,NUM) ->
   Opt = [list, {active, false}, {reuseaddr, true}],
+  registers_handlers(Port,Opt,NUM).
+
+registers_handlers(_,_,0) ->
+  ok;
+
+registers_handlers(Port,Opt,NUM) ->
+  Pid = spawn(rudy,throwHandler,[Port,Opt]),
+  lists:append(?Pids,Pid),
+  registers_handlers(Port,Opt,NUM-1).
+
+throwHandler(Port,Opt) ->
   case gen_tcp:listen(Port, Opt) of
     {ok, Listen} ->
       handler(Listen),
       gen_tcp:close(Listen),
       ok;
-    {error, Error} ->
+    {error, _} ->
       error
-end.
+  end.
 
 handler(Listen) ->
   case gen_tcp:accept(Listen) of
     {ok, Client} ->
       request(Client);
-    {error, Error} ->
+    {error, _} ->
       error
   end,
   handler(Listen).
@@ -44,5 +70,4 @@ request(Client) ->
 
 
 reply({{get, URI, _}, _, _}) ->
-  timer:sleep(40),
   http:ok(URI).
