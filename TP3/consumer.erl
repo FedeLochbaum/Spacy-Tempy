c391eb21a8cc6e6bae51d4104f200acc9cddd97c
Federico Lochbaum <federico.lochbaum@gmail.com>
2017-04-07 00:14:34 -0300

    punto 2 terminado

--- TP3/consumer.erl
+++ TP3/consumer.erl
@@ -1,18 +1,19 @@
 -module(consumer).
--export([start/1, stop/0, init/1, recursiveFunction/1]).
+-export([start/1, stop/0, init/1, consumer/2]).
 
-start(producer) ->
-    Consumer = spawn(fun() -> init(producer) end),
+start(Producer) ->
+    Consumer = spawn(fun() -> init(Producer) end),
     register(consumer, Consumer).
 
 stop() ->
     consumer ! stop.
 
-init(producer) ->
-    producer ! {hello, self()},
-    recursiveFunction(0).
+init(Producer) ->
+    Monitor = monitor(process, Producer),
+    Producer ! {hello, self()},
+    consumer(0, Monitor).
 
-recursiveFunction(N) ->
+consumer(N,Monitor) ->
     receive
         {ping, M} ->
             if
@@ -20,7 +21,10 @@ recursiveFunction(N) ->
               M>N  -> io:format(warning);
               true -> io:format("Error !")
             end,
-              recursiveFunction(M+1);
+              consumer(M+1,Monitor);
          bye ->
-              stop()
+              stop();
+        {'DOWN', Monitor, process, Object, Info} ->
+              io:format("~w died; ~w~n", [Object, Info]),
+              consumer(N, Monitor)
     end.
